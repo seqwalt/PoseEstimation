733788aa72c1d5e4388162774e6763d2fb83a6f8
pythonw-exe <26618979+pythonw-exe@users.noreply.github.com>
2022-12-08 01:05:14 -0600

    Add some docs for Evaluation.py

--- src/evaluation/Evaluation.py
+++ src/evaluation/Evaluation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from sklearn.neighbors import KDTree
 
 def translation_error(t_est, t_gt):
     t_error = np.linalg.norm(t_gt - t_est)/np.linalg.norm(t_est)
@@ -14,11 +15,11 @@ def rotation_error(r_est, r_gt):
     return r_error
 
 def compute_add_score(pts3d, diameter, R_pred, t_pred, R_gt, t_gt, percentage=0.1):
-    count = R_gt.shape[0]
+    count = pts3d.shape[0]
     mean_distances = np.zeros((count,), dtype=np.float32)
     for i in range(count):
-        pts_xformed_gt = R_gt[i] * pts3d.transpose() + t_gt[i]        
-        pts_xformed_pred = R_pred[i] * pts3d.transpose() + t_pred[i]
+        pts_xformed_gt = R_gt * pts3d[i].transpose() + t_gt.transpose() 
+        pts_xformed_pred = R_pred * pts3d[i].transpose() + t_pred.transpose()
         distance = np.linalg.norm(pts_xformed_gt - pts_xformed_pred, axis=0)
         mean_distances[i] = np.mean(distance)            
 
@@ -26,18 +27,59 @@ def compute_add_score(pts3d, diameter, R_pred, t_pred, R_gt, t_gt, percentage=0.
     score = (mean_distances < threshold).sum() / count
     return score
 
-def compute_adds_score(pts3d, diameter, pose_gt, pose_pred, percentage=0.1):
-    count = R_gt.shape[0]
+def compute_adds_score(pts3d, diameter, R_pred, t_pred, R_gt, t_gt, percentage=0.1):
+    count = pts3d.shape[0]
     mean_distances = np.zeros((count,), dtype=np.float32)
     for i in range(count):
-        if np.isnan(np.sum(t_pred[i])):
-            mean_distances[i] = np.inf
-            continue
-        pts_xformed_gt = R_gt[i] * pts3d.transpose() + t_gt[i]
-        pts_xformed_pred = R_pred[i] * pts3d.transpose() + t_pred[i]
+        pts_xformed_gt = R_gt * pts3d[i].transpose() + t_gt.transpose()
+        pts_xformed_pred = R_pred * pts3d[i].transpose() + t_pred.transpose()
         kdt = KDTree(pts_xformed_gt.transpose(), metric='euclidean')
         distance, _ = kdt.query(pts_xformed_pred.transpose(), k=1)
         mean_distances[i] = np.mean(distance)
     threshold = diameter * percentage
     score = (mean_distances < threshold).sum() / count
     return score
+
+
+#example for translation error 
+#gournd truth
+t_gt0 = np.array([3, 2, 1])
+#estimation
+t_est1 = np.array([[3, 2, 1]]) 
+t_est2 = np.array([[4, 5, 6]])
+
+#translation error
+t_err1 = translation_error(t_est1, t_gt0)
+t_err2 = translation_error(t_est2, t_gt0)
+
+print("translation error")
+print(t_err1, t_err2)
+
+#example for rotation error
+#ground truth
+r_gt0 = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+#estmation 
+r_est1 = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+r_est2 = np.array([[1, 0, 0], [ 0, 0, -1],[ 0, 1, 0]])
+
+#rotation error
+r_err1 = rotation_error(r_est1, r_gt0)
+r_err2 = rotation_error(r_est2, r_gt0)
+
+print("rotation error")
+print (r_err1, r_err2)
+
+#Example for ADD(-S)
+#sample points
+pts3d0 = np.array([[100,100,100],[5,10,10],[1,2,3],[2,2,2]])
+#model diameter
+diameter0 = 100
+
+add1 = compute_add_score(pts3d0, diameter0, r_est1, t_est1, r_gt0, t_gt0)
+adds1 = compute_adds_score(pts3d0, diameter0, r_est1, t_est1, r_gt0, t_gt0)
+
+add2 = compute_add_score(pts3d0, diameter0, r_est2, t_est2, r_gt0, t_gt0)
+adds2 = compute_adds_score(pts3d0, diameter0, r_est2, t_est2, r_gt0, t_gt0)
+
+print("ADD & ADD-s")
+print (add1, adds1, add2, adds2)
